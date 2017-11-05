3c6195849e33cda5881c0a0bba0c387a1558a462
Joakim Antman <antmanj@gmail.com>
2017-11-05 09:56:50 +0200

    Removed codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-else
-  require 'simplecov'
-  SimpleCov.start
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec/'
 end
 require 'pry'
 require 'vcr'
